ab7f57d6da83134a3f5fd0adcb8924cad7bc0fd6
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-19 09:44:59 +0900

    update

--- basic.sql
+++ basic.sql
@@ -11,3 +11,8 @@ SELECT * FROM users WHERE id != 1;
 -- usersテーブルから、id >= 50 のユーザを選択
 SELECT * FROM users WHERE id >= 50;
 
+-- usersテーブルから、id >= 5 and id < 10 のユーザを選択
+SELECT * FROM users WHERE id >= 5 AND id < 10;
+
+-- usersテーブルから、id = 5 or id = 10 のユーザを選択
+SELECT * FROM users WHERE id = 5 OR id = 10;
