caac032b193051183b494a7f843868f9ba5d7416
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-19 09:37:34 +0900

    update

--- basic.sql
+++ basic.sql
@@ -1,6 +1,13 @@
+-- SELECT(レコード選択)
 -- usersテーブルのすべてのレコードを選択
 SELECT * FROM users;
 
 -- usersテーブルから、id = 1 のユーザを選択
 SELECT * FROM users WHERE id = 1;
+
+-- usersテーブルから、id = 1 以外のユーザを選択
+SELECT * FROM users WHERE id != 1;
+
+-- usersテーブルから、id >= 50 のユーザを選択
+SELECT * FROM users WHERE id >= 50;
 
