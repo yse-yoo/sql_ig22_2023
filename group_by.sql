6a01dd25fcbbafd07b74c6f3f9d7fd612ba50d80
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-26 08:46:50 +0900

    add group by

---
+++ group_by.sql
@@ -0,0 +1,22 @@
+-- user_itemsテーブルの集計
+
+SELECT user_id FROM user_items;
+
+-- user_idでグループ
+SELECT user_id 
+FROM user_items
+GROUP BY user_id;
+
+-- user_idでグループ
+SELECT 
+    user_id,
+    COUNT(user_Id) AS user_count
+FROM user_items
+GROUP BY user_id;
+
+SELECT 
+    user_id,
+    COUNT(user_Id) AS user_count,
+    SUM(total_price) AS total_price
+FROM user_items
+GROUP BY user_id;
