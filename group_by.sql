2fad9bb54151a575b29c20347acdd3078d5e8bc8
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-26 09:42:15 +0900

    update

--- group_by.sql
+++ group_by.sql
@@ -10,13 +10,13 @@ GROUP BY user_id;
 -- user_idでグループ
 SELECT 
     user_id,
-    COUNT(user_Id) AS user_count
+    COUNT(user_id) AS item_count
 FROM user_items
 GROUP BY user_id;
 
 SELECT 
     user_id,
-    COUNT(user_Id) AS user_count,
+    COUNT(user_id) AS item_count,
     SUM(total_price) AS total_price
 FROM user_items
 GROUP BY user_id;
