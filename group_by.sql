a14fc641967744c4a1fcea95414dcc32a54fe752
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-26 10:28:27 +0900

    update

--- group_by.sql
+++ group_by.sql
@@ -31,6 +31,17 @@ FROM user_items
 GROUP BY item_id;
 
 -- JOIN
+-- 「user_items」に「users」を内部結合（INNER JOIN）
+SELECT 
+    users.id,
+    users.name,
+    user_items.user_id,
+    user_items.item_id,
+    user_items.created_at
+FROM user_items
+INNER JOIN users ON user_items.user_id = users.id;
+
+-- INNERを省略
 SELECT 
     users.id,
     users.name,
