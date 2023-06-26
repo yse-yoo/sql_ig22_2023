34d16657fc3fafac43462d884ba088bc7ef4ca90
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-26 14:04:23 +0900

    update

--- subquery.sql
+++ subquery.sql
@@ -1,4 +1,5 @@
--- 「users.gender = female」の「user_items」のデータを表示
+-- WHEREでサブクエリ
+--「users.gender = female」の「user_items」のデータを表示
 
 SELECT * FROM user_items
 WHERE user_items.user_id IN (SELECT id FROM users WHERE gender = "female");
@@ -21,3 +22,14 @@ SELECT
 FROM user_items
 JOIN users ON user_items.user_id = users.id
 WHERE user_items.user_id IN (SELECT id FROM users WHERE gender = "male");
+
+-- FROMでサブクエリ
+--「users.gender = female」の「user_items」のデータを表示
+SELECT 
+    female_users.id AS user_id,
+    female_users.name,
+    user_items.id AS user_items_id,
+    user_items.item_id,
+    user_items.amount
+FROM (SELECT * FROM users WHERE gender = "female") AS female_users
+JOIN user_items ON female_users.id = user_items.user_id;
