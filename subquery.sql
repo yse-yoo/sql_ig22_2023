7ce91479b093d25841ea23bb7a2d9fb919051e1a
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-26 13:48:05 +0900

    update

--- subquery.sql
+++ subquery.sql
@@ -1,8 +1,23 @@
 -- 「users.gender = female」の「user_items」のデータを表示
 
 SELECT * FROM user_items
-WHERE user_items.id IN (SELECT id FROM users WHERE gender = "female");
+WHERE user_items.user_id IN (SELECT id FROM users WHERE gender = "female");
 
 -- 「users.gender = male」の「user_items」のデータを表示
 SELECT * FROM user_items
-WHERE user_items.id IN (SELECT id FROM users WHERE gender = "male");
+WHERE user_items.user_id IN (SELECT id FROM users WHERE gender = "male");
+
+-- JOINを追加
+SELECT 
+    users.gender,
+    user_items.*
+FROM user_items
+JOIN users ON user_items.user_id = users.id
+WHERE user_items.user_id IN (SELECT id FROM users WHERE gender = "female");
+
+SELECT 
+    users.gender,
+    user_items.*
+FROM user_items
+JOIN users ON user_items.user_id = users.id
+WHERE user_items.user_id IN (SELECT id FROM users WHERE gender = "male");
